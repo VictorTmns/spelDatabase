9b4bd4bcd5d39507f82864b675651e5ca9986f94
Bemiel <107100116+Bemiel@users.noreply.github.com>
2022-06-28 15:45:36 +0200

    Spel.php
    begin van de spel.php website (de display van normale spelen vanuit de database)

---
+++ app/spel.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Verzameling van alle spelen op één website">
+    <meta name="keywords" content="Spel, Spelen, Databse, Scouts">
+    <meta name="author" content="Simon De Clerck en Victor Taeijmans">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/nav_footer.css">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Spel Database</title>
+</head>
+<body>
+    <?php include("includes/nav.inc.php"); ?>
+    <main>
+        <h1>Dikke Bertha</h1>
+        <p>Houd jij van overlopen en weglopen van dikke bertha? Ren dan al maar snel weg want dikke bertha komt je halen! In dit spel ren je heen en weer om dikke bertha te ontwijken, en geen gemiddeld amerikaans postuur te krijgen!</p>
+        <div class="row img-row">
+            <img class="spel-img" src="images/dickebertha.png">
+            <img class="spel-img" src="images/dickebertha.png">
+        </div>
+        <h1>Speluitleg</h1>
+        <br>
+        <h2>Lopen</h2>
+        <p>Een essentieel deel van dikke bertha is lopen. Zonder te lopen ben je namelijk nergens, behalve in de armen van DE DIKKE BERTHA. en om dit te vermijden, lopen we. Hoe moet je lopen? Met je benen!</p>
+        <br>
+        <h2>Regels</h2>
+        <p>Het spel is 'vree' simpel. je loopt op signaal naar de andere kant van het speelveld en vermijd de dikke berthas die je proberen te vangen en op te tillen. Lukt dit? Herhaal stap 1. Ben je gevangen? Gefeliciteerd! Je bent zonet verdubbeld in BMI en een officiele mensen vanger geworden.</p>
+        <br>
+        <h2>Varianten</h2>
+        <p>Dat pakken is soms toch maar saai. En ja geef maar toe dat je tijdens het originele spel al hebt zitten tackelen. Dit mag uiteraard niet, maar speciaal voor de tackle-fans hebben wij nu <b>Platte Jan</b>. Hetzelfde spel, maar deze keer met mensen op de grond gooien. PRO TIP: Speel dit op een modderveld voor dubbele fun!</p>
+    </main>
+    <?php include("includes/footer.inc.php"); ?>
+</body>
+</html>
