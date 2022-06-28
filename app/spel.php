8644994de68019f48e05e1bb156d0de922d0dc5c
Bemiel <107100116+Bemiel@users.noreply.github.com>
2022-06-28 17:16:01 +0200

    spel.php imporvements
    verdere uitwerking van spel.php

--- app/spel.php
+++ app/spel.php
@@ -12,25 +12,68 @@
     <title>Spel Database</title>
 </head>
 <body>
-    <?php include("includes/nav.inc.php"); ?>
-    <main>
-        <h1>Dikke Bertha</h1>
-        <p>Houd jij van overlopen en weglopen van dikke bertha? Ren dan al maar snel weg want dikke bertha komt je halen! In dit spel ren je heen en weer om dikke bertha te ontwijken, en geen gemiddeld amerikaans postuur te krijgen!</p>
-        <div class="row img-row">
-            <img class="spel-img" src="images/dickebertha.png">
-            <img class="spel-img" src="images/dickebertha.png">
+<?php include("includes/nav.inc.php"); ?>
+<main>
+
+<div class="spel-content">
+    <h1>Dikke Bertha</h1>
+    <p>Houd jij van overlopen en weglopen van dikke bertha? Ren dan al maar snel weg want dikke bertha komt je halen! In dit spel ren je heen en weer om dikke bertha te ontwijken, en geen gemiddeld amerikaans postuur te krijgen!</p>
+    <img class="spel-img" src="images/dickebertha.png">
+    <img class="spel-img" src="images/dickebertha.png">
+    <h1>Speluitleg</h1>
+    <br>
+    <h2>Lopen</h2>
+    <p>Een essentieel deel van dikke bertha is lopen. Zonder te lopen ben je namelijk nergens, behalve in de armen van DE DIKKE BERTHA. en om dit te vermijden, lopen we. Hoe moet je lopen? Met je benen!</p>
+    <br>
+    <h2>Regels</h2>
+    <p>Het spel is 'vree' simpel. je loopt op signaal naar de andere kant van het speelveld en vermijd de dikke berthas die je proberen te vangen en op te tillen. Lukt dit? Herhaal stap 1. Ben je gevangen? Gefeliciteerd! Je bent zonet verdubbeld in BMI en een officiele mensen vanger geworden.</p>
+    <br>
+    <h2>Varianten</h2>
+    <p>Dat pakken is soms toch maar saai. En ja geef maar toe dat je tijdens het originele spel al hebt zitten tackelen. Dit mag uiteraard niet, maar speciaal voor de tackle-fans hebben wij nu <b>Platte Jan</b>. Hetzelfde spel, maar deze keer met mensen op de grond gooien. PRO TIP: Speel dit op een modderveld voor dubbele fun!</p>
+</div>
+
+<div class="andere-spelen">
+    <h2>Gelijkaardige Spelen</h2>
+    <br>
+    <!-- Spel-Previeuw insert van spelen met gelijkaardige tags? -->
+    <div class="spel-previeuw">
+        <h2>Trugby</h2>
+        <br>
+        <p>Deze klassieker combineert trefbal met rugby. Vandaar de naam. Snap je? Trefbal + Rugby = Trugby? Laat maar. leuk spel tho</p>
+        <br>
+        <div class="tags">
+            <div class="tag"><p>Balspel</p></div>
+            <div class="tag"><p>Trefbal</p></div>
+            <div class="tag"><p>2 Teams</p></div>
         </div>
-        <h1>Speluitleg</h1>
+    </div>
+
+    <div class="spel-previeuw">
+        <h2>Schipper mag ik overvaren?</h2>
         <br>
-        <h2>Lopen</h2>
-        <p>Een essentieel deel van dikke bertha is lopen. Zonder te lopen ben je namelijk nergens, behalve in de armen van DE DIKKE BERTHA. en om dit te vermijden, lopen we. Hoe moet je lopen? Met je benen!</p>
+        <p>Overloop-spel waarbij je enkel over mag als je niet binnen de gediscrimineerde-kleren-categorie zit. In thema van een... ...schipper? Waarom doet iedereen alsof dit het normaalste spel ooit is?</p>
         <br>
-        <h2>Regels</h2>
-        <p>Het spel is 'vree' simpel. je loopt op signaal naar de andere kant van het speelveld en vermijd de dikke berthas die je proberen te vangen en op te tillen. Lukt dit? Herhaal stap 1. Ben je gevangen? Gefeliciteerd! Je bent zonet verdubbeld in BMI en een officiele mensen vanger geworden.</p>
+        <div class="tags">
+            <div class="tag"><p>Loopspel</p></div>
+            <div class="tag"><p>2 Teams</p></div>
+            <div class="tag"><p>Vangen of tikken</p></div>
+        </div>
+    </div>
+
+    <div class="spel-previeuw">
+        <h2>Levende Rumikub</h2>
         <br>
-        <h2>Varianten</h2>
-        <p>Dat pakken is soms toch maar saai. En ja geef maar toe dat je tijdens het originele spel al hebt zitten tackelen. Dit mag uiteraard niet, maar speciaal voor de tackle-fans hebben wij nu <b>Platte Jan</b>. Hetzelfde spel, maar deze keer met mensen op de grond gooien. PRO TIP: Speel dit op een modderveld voor dubbele fun!</p>
-    </main>
-    <?php include("includes/footer.inc.php"); ?>
+        <p>Zijn gezelschapspelen te 'dood' voor jou? Levende Rumikub zal even dood zijn, maar het woord 'levend' komt in de naam dus haha grappig. <b>nee</b>.</p>
+        <br>
+        <div class="tags">
+            <div class="tag"><p>Denksport</p></div>
+            <div class="tag"><p>Intensief</p></div>
+            <div class="tag"><p>Strategisch</p></div>
+            <div class="tag"><p>Scuffed</p></div>
+        </div>
+    </div>
+</div>
+</main>
+<?php include("includes/footer.inc.php"); ?>
 </body>
 </html>
