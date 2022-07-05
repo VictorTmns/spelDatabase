bdd91898d7b284c05876eee790315ec72ef37c11
Victortaeijmans <victortaeijmans@gmail.com>
2022-07-05 18:12:36 +0200

    toevoegen spelFotos spelDatabase.sql
    de speldatabase heeft nu een tafel voor fotoURLs op te slagen genaamd "spelFotos". Deze heeft een voorbeeldpopulatie en een relatie naar de tafel "spelen"

--- spelDatatabase.sql
+++ spelDatatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 05, 2022 at 04:41 PM
+-- Generation Time: Jul 05, 2022 at 05:10 PM
 -- Server version: 8.0.26-0ubuntu0.20.04.2
 -- PHP Version: 8.0.10
 
@@ -43,6 +43,26 @@ CREATE TABLE `spelen` (
 INSERT INTO `spelen` (`SpelID`, `spelnaam`, `korte beschrijving`, `lange beschrijving`, `variant`, `goedgekeurd`) VALUES
 ('Test01', 'een testspelletje', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec dapibus diam. Nunc pharetra a urna vel tristique. Sed id dapibus magna. Duis sit amet justo at augue sagittis vulputate sed vitae elit.\r\n', 'Nunc varius, diam id euismod tempor, erat urna tempor orci, fermentum elementum quam dui et eros. Phasellus rhoncus ex massa, ac placerat dolor viverra in. Phasellus placerat nisi erat, non posuere elit ullamcorper nec. Ut sed sagittis nisl. Curabitur eget libero dolor. Fusce blandit volutpat nisi eget pellentesque. Donec facilisis porta mauris, sed imperdiet lorem lobortis congue. Mauris rhoncus diam enim. Nulla convallis dolor elementum nunc posuere, sit amet pulvinar elit fermentum. Donec et leo in nulla bibendum tempus. Quisque vel tellus ut lectus interdum dignissim eu id leo. Proin libero ligula, malesuada non turpis ut, scelerisque dignissim dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc eu quam vitae odio posuere malesuada. In ac venenatis tortor. Suspendisse eleifend neque id nunc tempor, sit amet lobortis eros facilisis.\r\n\r\n', 'Nullam tincidunt semper nunc, sagittis lobortis purus imperdiet non. Fusce faucibus pharetra libero eget consequat. Proin ex turpis, interdum at fermentum eget, faucibus sed est. Duis mollis rhoncus blandit. Phasellus nec efficitur lacus. Praesent pharetra vulputate ornare. Duis ante dui, pretium nec pellentesque at, congue consectetur ante. Duis ornare metus ac lorem suscipit tristique. Pellentesque diam lectus, facilisis nec velit et, tincidunt suscipit justo.\r\n\r\n', 1),
 ('Test02', 'een testspelletje2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta tristique vehicula. Ut sit amet dolor lorem. Duis posuere tellus eu erat accumsan placerat. Nam ante augue, blandit vitae ex ut, luctus aliquet dolor. Ut at arcu consequat, vulputate metus nec, fringilla nisi.', 'Curabitur semper elit porta metus porta, non efficitur eros bibendum. Praesent dapibus dolor eget odio rhoncus eleifend. Nunc in purus semper, imperdiet nulla porta, condimentum est. Curabitur scelerisque mauris vel leo sollicitudin, sagittis volutpat odio lacinia. Vestibulum erat orci, porttitor id dui sit amet, efficitur egestas felis. Nunc libero eros, molestie eu ante quis, lacinia sodales ipsum. Morbi porta nulla dolor, eu lacinia erat suscipit id. Nam ornare eros mi, vitae posuere eros blandit quis. Pellentesque enim nisl, posuere vel tempor ut, efficitur sed ante.\r\n\r\n', 'Duis pharetra id justo in viverra. Donec vel ligula est. Morbi orci mi, accumsan vel dui eu, sodales rutrum ante. Maecenas odio massa, mollis vel elit eu, vehicula ultricies sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent fringilla lectus et nisi elementum mattis. Duis porttitor nec diam et tempus. Integer quis laoreet nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi diam nunc, sodales dignissim vestibulum quis, vulputate vel mauris. Etiam sed dictum ante.\r\n\r\n', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spelFotos`
+--
+
+CREATE TABLE `spelFotos` (
+  `spelID` varchar(63) NOT NULL,
+  `fotoFileName` varchar(127) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `spelFotos`
+--
+
+INSERT INTO `spelFotos` (`spelID`, `fotoFileName`) VALUES
+('Test01', 'Testfoto01'),
+('Test01', 'Testfoto02'),
+('Test02', 'Testfoto03');
 
 -- --------------------------------------------------------
 
@@ -104,6 +124,12 @@ ALTER TABLE `spelen`
   ADD PRIMARY KEY (`SpelID`);
 
 --
+-- Indexes for table `spelFotos`
+--
+ALTER TABLE `spelFotos`
+  ADD PRIMARY KEY (`spelID`,`fotoFileName`);
+
+--
 -- Indexes for table `spelTags`
 --
 ALTER TABLE `spelTags`
@@ -119,6 +145,12 @@ ALTER TABLE `tags`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `spelFotos`
+--
+ALTER TABLE `spelFotos`
+  ADD CONSTRAINT `spelfotos_ibfk_1` FOREIGN KEY (`spelID`) REFERENCES `spelen` (`SpelID`);
 
 --
 -- Constraints for table `spelTags`
